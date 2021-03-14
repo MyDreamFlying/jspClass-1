fb383a556794d9ab9acb68de6c6b66ad0ce8947c
ShanePark <psh40963@naver.com>
2021-03-14 16:56:27 +0900

    removed concat method

--- webStudy01/src/kr/or/ddit/servlet01/textFormServlet.java
+++ webStudy01/src/kr/or/ddit/servlet01/textFormServlet.java
@@ -10,12 +10,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// 1. 텍스트 파일만 옵션으로 제공 할것
-// 2. 텍스트 파일 체인지 이벤트로 , iframe에 그 내용이 출력 되도록
-
 @WebServlet("/01/textViewer.tmpl")
 public class textFormServlet extends AbstractUseTmplServlet{
-
 	private static final long serialVersionUID = 1L;
 
 	@Override
@@ -25,23 +21,19 @@ public class textFormServlet extends AbstractUseTmplServlet{
 
 	@Override
 	protected void makeData(HttpServletRequest req) {
-
 		URL path = getClass().getResource("/datas");
 		File contents = new File(path.getFile());
-
 		ArrayList<String> children = getChildren(contents);
-
 		StringBuffer options = new StringBuffer();
+		
 		for(String child : children){
 			options.append(String.format("<option>%s</option>",child));
 		}
 		req.setAttribute("options",options);
-
 	}
 
 	private ArrayList<String>  getChildren(File contents) {
 		ArrayList<String> list = new ArrayList<>();
-
 		File[] children = contents.listFiles();
 		
 		for(File child : children) {
@@ -49,7 +41,6 @@ public class textFormServlet extends AbstractUseTmplServlet{
 				list.addAll(getChildren(child));
 			}else {
 				String mime = application.getMimeType(child.getName());
-				
 				if(mime!=null && mime.startsWith("text/"))
 					list.add(String.format("%s%s", getParent(child),child.getName()));
 			}
@@ -59,13 +50,13 @@ public class textFormServlet extends AbstractUseTmplServlet{
 	
 	private StringBuffer getParent(File file) {
 		StringBuffer parent = new StringBuffer();
+		
 		if(!"datas".equals(file.getParentFile().getName())) {
-			parent.insert(0,getParent(file.getParentFile()) + file.getParentFile().getName());
-			parent.append("/");
+			StringBuffer ancestors = getParent(file.getParentFile());
+			String parentDir = file.getParentFile().getName();
+			parent.insert(0,ancestors.append(parentDir).append("/"));
 		}
 		return parent;
-		
 	}
 
 }
-
