972a541e588490dc8fadaf12d3864e168fe9db6c
ShanePark <psh40963@naver.com>
2021-03-30 17:54:31 +0900

    jdbc without Mybatis practice

--- webStudy01/src/kr/or/ddit/login/LoginCheckServlet.java
+++ webStudy01/src/kr/or/ddit/login/LoginCheckServlet.java
@@ -10,9 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import kr.or.ddit.enumpkg.ServiceResult;
+import kr.or.ddit.member.service.AuthenticateServiceImpl;
+import kr.or.ddit.member.service.IAuthenticateService;
+import kr.or.ddit.vo.MemberVO;
+
 @WebServlet("/login/loginCheck.do")
 public class LoginCheckServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
+	private IAuthenticateService service = new AuthenticateServiceImpl();
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -43,23 +49,33 @@ public class LoginCheckServlet extends HttpServlet{
 		boolean redirect = false;
 		boolean valid = validate(mem_id, mem_pass);
 		String message = null;
+		MemberVO member = new MemberVO(mem_id, mem_pass);
 		
 		if(valid) {
 		// 인증 (id == password)
-			boolean auth = authenticate(mem_id, mem_pass);
-			if(auth) {
-				// 인증 성공시 index.jsp로 이동 (현재 요청 정보 삭제, 오류메시지 삭제)
+			ServiceResult result = service.authenticate(member);
+			
+			switch(result) {
+			case OK:
 				redirect = true;
 				view = "/";
-				session.setAttribute("authId", mem_id);
+				session.setAttribute("authMember", member);
 				session.setAttribute("message", "");
-			}else {
+				break;
+			case NOTEXIST:
+				redirect = true;
+				view = "/login/loginForm.jsp";
+				message = "아이디 오류";
+				break;
+			case INVALIDPASSWORD:
 				redirect = true;
 				// 인증 실패시 loginForm.jsp로 이동
 				view = "/login/loginForm.jsp";
 				// 2) 인증 실패
 				message = "비번 오류";
+				break;
 			}
+			
 		}else {
 			// 1)검증
 			redirect = true;
@@ -80,12 +96,8 @@ public class LoginCheckServlet extends HttpServlet{
 			req.getRequestDispatcher(view).forward(req,resp);
 		}
 		
-		
 	}
 
-	private boolean authenticate(String mem_id, String mem_pass) {
-		return mem_id.equals(mem_pass);
-	}
 
 	private boolean validate(String mem_id, String mem_pass) {
 		boolean valid = true;
--- webStudy01/webapps/index.jsp
+++ webStudy01/webapps/index.jsp
@@ -1,3 +1,4 @@
+<%@page import="kr.or.ddit.vo.MemberVO"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -9,11 +10,20 @@
 <body>
 <h4>Welcome page!</h4>
 <%
-	String authId = (String) session.getAttribute("authId");
-	if(authId != null && !authId.isEmpty()){
+	MemberVO authMember = (MemberVO) session.getAttribute("authMember");
+	String authId = "";
+	String memName = "";
+	String email = "";
+	if(authMember != null){
+		authId = authMember.getMem_id();
+		memName = authMember.getMem_name();
+		email = authMember.getMem_mail();
+		if(authId != null && !authId.isEmpty()){
+	}
 		%>
 		<form name="logoutForm" method="post" action="<%=request.getContextPath() %>/login/logout.do"></form>
-		<%=authId %>님 로그인 성공!
+		<a href="<%=request.getContextPath() %>/mypage.do"><%=memName %></a>님 로그인 성공!
+		<a href="mailto:<%=email %>">이메일보내기</a>
 		<a href="#" onClick="clickHandler(event);">로그아웃</a>
 		<script type="text/javascript">
 			function clickHandler(event){
