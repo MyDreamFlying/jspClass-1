6b91952c8754e362791f42a7f848d245f8dc1e49
ShanePark <psh40963@naver.com>
2021-04-16 23:09:49 +0900

    ALBA : FrontController Pattern

--- alba/alba/src/main/java/kr/or/ddit/test/Test05Annotation.java
+++ alba/alba/src/main/java/kr/or/ddit/test/Test05Annotation.java
@@ -7,23 +7,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.or.ddit.mvc.annotation.Controller;
+import kr.or.ddit.mvc.annotation.RequestMapping;
 import kr.or.ddit.utils.db.ConnectionFactory;
 
-@WebServlet("/test04")
-public class Test04Servlet extends HttpServlet{
+@Controller
+public class Test05Annotation extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@RequestMapping("/test05.do")
+	public String test05(HttpServletResponse resp) throws IOException{
 		resp.setContentType("text/html; charset=utf-8");
 		try(
 				Connection conn = ConnectionFactory.getConnection();
+				PrintWriter out = resp.getWriter();
 			){
 				String sql = "select * from alba";
 				Statement statement = conn.createStatement();
@@ -44,7 +44,7 @@ public class Test04Servlet extends HttpServlet{
 					String al_spec = result.getString("al_spec");
 					String al_desc = result.getString("al_desc");
 					String al_img = result.getString("al_img");
-					PrintWriter out = resp.getWriter();
+					
 					String resultString = String.format("%s %s %d %s %s %s %s %s %s %s %s %s %s %s",al_id,al_name,al_age,al_zip,al_addr1,al_addr2,al_hp,gr_code,al_gen,al_mail,al_career,al_spec,al_desc,al_img);
 					out.write(resultString);
 					out.write("<br>");
@@ -53,6 +53,7 @@ public class Test04Servlet extends HttpServlet{
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+		return null;
 	}
 	
 }
